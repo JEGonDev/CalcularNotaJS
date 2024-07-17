5fee00719909b848def23c6599ec5d1ee5aae9b0
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-07-17 13:36:15 -0500

    finalizado

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ Variables let
 Constantes const
 Ventana emergente prompt
 Alerta emergente alert 
+
+// 1. Rango de la nota entre 1 y 5.
+// 2. Máximo dos decimales.
+// 3. La materia se gana con 3.5 o más.
+// 4. Si la nota es 5 -> felicitaciones con el nombre del estudiante.
+// 5. n1 * 0.3 + n2 * 0.3 + n1 * 0.4
+// 6. Si la nota es igual o mayor a 3.5 hasta 4.99 mensaje en color verde.
+// 7. Si reprueba en color rojo para el mensaje.
+
 */
 
 let username = prompt("Por favor ingrese su nombre")
@@ -18,13 +27,22 @@ let pResolve = document.getElementById("p-resolve")
 
 frmNotes.addEventListener("submit", calculeNotes)
 
-function calculeNotes(event){
+function calculeNotes(event) {
     event.preventDefault() // No elimina los datos de los input despues de hacer el submit
-    let data1 = note1.value
-    let data2 = note2.value
-    let data3 = note3.value
+    let data1 = parseFloat(note1.value) * 0.3
+    let data2 = parseFloat(note2.value) * 0.3
+    let data3 = parseFloat(note3.value) * 0.4
 
-    let response = 
+    let response = (data1 + data2 + data3).toFixed(2)
 
-    pResolve.textContent = ` Data1: ${typeof data1}`
+    if (response < 3.5) {
+        pResolve.textContent = `SR@ ${username}, su nota reprobada es: ${response}`
+        pResolve.style.color = 'red'
+    } else if (response >= 3.5 && response < 5) {
+        pResolve.textContent = `SR@ ${username}, su nota aprobada es: ${response}`
+        pResolve.style.color = 'green'
+    } else {
+        pResolve.textContent = `SR@ ${username}, Felicitaciones, ha alcanzado la nota superior, ${response}`
+        pResolve.style.color = 'orange'
+    }
 }
